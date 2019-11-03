d7484aa692ad5646c1ef3b2d4dfcdc3803eca81b
mr_chepel <aleks.shev@gmail.com>
2019-11-03 11:53:22 +0300

    add ls

--- 20_asynchronous_programming/challenges/2_ls/__tests__/test.js
+++ 20_asynchronous_programming/challenges/2_ls/__tests__/test.js
@@ -7,13 +7,16 @@ test('ls dir', async () => {
   const expected = [
     { filepath: '/var/backups', mode: 16877 },
     { filepath: '/var/cache', mode: 16877 },
+    { filepath: '/var/crash', mode: 17407 },
     { filepath: '/var/lib', mode: 16877 },
     { filepath: '/var/local', mode: 17917 },
     { filepath: '/var/lock', mode: 17407 },
-    { filepath: '/var/log', mode: 16877 },
+    { filepath: '/var/log', mode: 16893 },
     { filepath: '/var/mail', mode: 17917 },
     { filepath: '/var/opt', mode: 16877 },
+    { filepath: '/var/ramfs', mode: 17407 },
     { filepath: '/var/run', mode: 16877 },
+    { filepath: '/var/snap', mode: 16877 },
     { filepath: '/var/spool', mode: 16877 },
     { filepath: '/var/tmp', mode: 17407 },
   ];
