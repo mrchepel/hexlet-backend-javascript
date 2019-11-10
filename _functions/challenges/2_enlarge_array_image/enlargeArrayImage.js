2e6079ce9c5b25d63dcc2e76838aa6c656a7909d
mr_chepel <aleks.shev@gmail.com>
2019-11-10 17:32:12 +0300

    fix bug

--- _functions/challenges/2_enlarge_array_image/enlargeArrayImage.js
+++ _functions/challenges/2_enlarge_array_image/enlargeArrayImage.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 export default (arr) => {
-  const mapped = (item) => _.flatten(item, (elem) => [elem, elem]);
+  const mapped = (item) => _.flatMap(item, (elem) => [elem, elem]);
   const reduced = (acc, item) => [...acc, mapped(item), mapped(item)];
   return arr.reduce(reduced, []);
 };
